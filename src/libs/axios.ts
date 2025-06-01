253ee97c031c2ba710f8f53f7ae8f95419111f27
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2025-06-01 21:58:03 +0700

    [refactor]: remove unused API_BASE_URL import and simplify axios baseURL

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { API_BASE_URL } from "./utils/env";
+// import { API_BASE_URL } from "./utils/env";
 
 export const axiosInstance = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: `/api`,
   headers: {
     "Content-Type": "application/json",
   },

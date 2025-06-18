0e7e93880610bbbfec975e5c778f7cd5351ce78e
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2025-06-18 13:44:22 +0700

    [refactor]: add getJabatan function to retrieve role names based on jabatanId; enhance role management utility

--- src/libs/utils/getRole.ts
+++ src/libs/utils/getRole.ts
@@ -14,3 +14,28 @@ export const getRole = (roleId: number) => {
       return "User Unknown";
   }
 };
+
+export const getJabatan = (jabatanId: string) => {
+  switch (jabatanId) {
+    case "koordinator_tu":
+      return "Koordinator TU";
+    case "dekan":
+      return "Dekan";
+    case "wakil_dekan_1":
+      return "Wakil Dekan I";
+    case "wakil_dekan_2":
+      return "Wakil Dekan II";
+    case "wakil_dekan_3":
+      return "Wakil Dekan III";
+    case "kaprodi":
+      return "Kaprodi";
+    case "arsiparis_surat_masuk":
+      return "Arsiparis Surat Masuk";
+    case "arsiparis_surat_keluar":
+      return "Arsiparis Surat Keluar";
+    case "staff":
+      return "Staff";
+    default:
+      return "User Unknown";
+  }
+};

7ee5ce5dd537da91779ec5cd19d8f23db508f57e
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2025-06-18 11:21:07 +0700

    [refactor]: reorganize imports and enhance readability in DashboardContainer; update role-based display logic for dashboard cards

--- src/components/ui/containers/DashboardContainer.tsx
+++ src/components/ui/containers/DashboardContainer.tsx
@@ -16,7 +16,11 @@ import SubHeader from "../headers/SubHeader";
 import TableData from "../table/TableData";
 
 // Icons
-import { TbCircleDashedLetterM, TbCircleDashedLetterK, TbCircleDashedLetterD } from "react-icons/tb";
+import {
+  TbCircleDashedLetterM,
+  TbCircleDashedLetterK,
+  TbCircleDashedLetterD,
+} from "react-icons/tb";
 
 interface LetterDetails {
   id: string;
@@ -41,11 +45,29 @@ export default function DashboardContainer() {
   const hasShownNotification = useRef(false);
   const { notify, contextHolder } = useNotify();
   const { isLoggedIn, clearIsLoggedIn, getUserId, getRole } = useAuthStore();
-  const { userMe, fetchUserManagementDataById } = useUserManagementStore();
+  const {
+    userMe,
+    userManagementData,
+    fetchUserManagementData,
+    fetchUserManagementDataById,
+  } = useUserManagementStore();
   const { classifierData, fetchClassifierData } = useClassifierStore();
-  const { letterData, countDataSuratMasuk, countDataSuratKeluar, countDataDisposisi, fetchCountSuratKeluar, fetchCountSuratMasuk, fetchCountDisposisi, fetchLetterData } = useDashboardStore();
+  const {
+    letterData,
+    countDataSuratMasuk,
+    countDataSuratKeluar,
+    countDataDisposisi,
+    fetchCountSuratKeluar,
+    fetchCountSuratMasuk,
+    fetchCountDisposisi,
+    fetchLetterData,
+  } = useDashboardStore();
   const userId = getUserId();
   const roleId = getRole();
+
+  const currentUser = userManagementData.find((user) => user.id === userId);
+  const isDekan =
+    currentUser?.jabatan && currentUser?.jabatan.toLowerCase() === "dekan";
 
   // show notification when isLoggedIn
   useEffect(() => {
@@ -68,12 +90,20 @@ export default function DashboardContainer() {
   }, [userId, fetchCountDisposisi]);
 
   useEffect(() => {
+    fetchUserManagementData();
     fetchUserManagementDataById();
     fetchCountSuratMasuk();
     fetchCountSuratKeluar();
     fetchLetterData();
     fetchClassifierData();
-  }, [fetchUserManagementDataById, fetchCountSuratMasuk, fetchCountSuratKeluar, fetchLetterData, fetchClassifierData]);
+  }, [
+    fetchUserManagementData,
+    fetchUserManagementDataById,
+    fetchCountSuratMasuk,
+    fetchCountSuratKeluar,
+    fetchLetterData,
+    fetchClassifierData,
+  ]);
 
   const datas = [
     {
@@ -93,7 +123,7 @@ export default function DashboardContainer() {
       count: countDataDisposisi,
       icon: <TbCircleDashedLetterD size={60} />,
       rolesAllowed: [1, 2, 3, 5],
-    }
+    },
   ];
 
   const processedData = useMemo(() => {
@@ -104,78 +134,86 @@ export default function DashboardContainer() {
     }));
   }, [letterData]);
 
-  const columns: ColumnsType<LetterDetails> = useMemo(() => [
-    {
-      title: "No",
-      dataIndex: "no",
-      key: "no",
-      width: 60,
-      align: "center",
-    },
-    {
-      title: "No Surat",
-      dataIndex: "no_surat",
-      key: "no_surat",
-    },
-    {
-      title: "Perihal",
-      dataIndex: "perihal_surat",
-      key: "perihal_surat",
-      width: 300,
-      render: (text) => <span className="text-ellipsis line-clamp-2">{text}</span>,
-    },
-    {
-      title: "Tanggal Surat",
-      dataIndex: "tanggal_surat",
-      key: "tanggal_surat",
-      width: 200,
-      render: (tanggal) =>
-        tanggal ? dayjs(tanggal).format("DD MMMM YYYY") : "-",
-    },
-    {
-      title: "Pengirim",
-      dataIndex: "pengirim_surat",
-      key: "pengirim_surat",
-      width: 150,
-      render: (text) => <span className="text-ellipsis line-clamp-2">{text}</span>,
-    },
-    {
-      title: "Penerima",
-      dataIndex: "penerima_surat",
-      key: "penerima_surat",
-      width: 150,
-      render: (text) => <span className="text-ellipsis line-clamp-2">{text}</span>,
-    },
-    {
-      title: "Jenis Surat",
-      dataIndex: "id_type_surat",
-      key: "id_type_surat",
-      render: (id) => {
-        const type = classifierData.find((item) => item.id === id);
-        return (
-          <Badge
-            color={type?.id === 1 ? "blue" : "green"}
-            text={type?.nama_type}
-            style={{ textTransform: "capitalize" }}
-          />
-        );
+  const columns: ColumnsType<LetterDetails> = useMemo(
+    () => [
+      {
+        title: "No",
+        dataIndex: "no",
+        key: "no",
+        width: 60,
+        align: "center",
       },
-    },
-    {
-      title: "Pengarsip",
-      dataIndex: "pengarsip",
-      key: "pengarsip",
-    },
-    {
-      title: "Tanggal Diarsipkan",
-      dataIndex: "created_at",
-      key: "created_at",
-      width: 200,
-      render: (tanggal) =>
-        tanggal ? dayjs(tanggal).format("DD MMMM YYYY") : "-",
-    },
-
-  ], [classifierData]);
+      {
+        title: "No Surat",
+        dataIndex: "no_surat",
+        key: "no_surat",
+      },
+      {
+        title: "Perihal",
+        dataIndex: "perihal_surat",
+        key: "perihal_surat",
+        width: 300,
+        render: (text) => (
+          <span className="text-ellipsis line-clamp-2">{text}</span>
+        ),
+      },
+      {
+        title: "Tanggal Surat",
+        dataIndex: "tanggal_surat",
+        key: "tanggal_surat",
+        width: 200,
+        render: (tanggal) =>
+          tanggal ? dayjs(tanggal).format("DD MMMM YYYY") : "-",
+      },
+      {
+        title: "Pengirim",
+        dataIndex: "pengirim_surat",
+        key: "pengirim_surat",
+        width: 150,
+        render: (text) => (
+          <span className="text-ellipsis line-clamp-2">{text}</span>
+        ),
+      },
+      {
+        title: "Penerima",
+        dataIndex: "penerima_surat",
+        key: "penerima_surat",
+        width: 150,
+        render: (text) => (
+          <span className="text-ellipsis line-clamp-2">{text}</span>
+        ),
+      },
+      {
+        title: "Jenis Surat",
+        dataIndex: "id_type_surat",
+        key: "id_type_surat",
+        render: (id) => {
+          const type = classifierData.find((item) => item.id === id);
+          return (
+            <Badge
+              color={type?.id === 1 ? "blue" : "green"}
+              text={type?.nama_type}
+              style={{ textTransform: "capitalize" }}
+            />
+          );
+        },
+      },
+      {
+        title: "Pengarsip",
+        dataIndex: "pengarsip",
+        key: "pengarsip",
+      },
+      {
+        title: "Tanggal Diarsipkan",
+        dataIndex: "created_at",
+        key: "created_at",
+        width: 200,
+        render: (tanggal) =>
+          tanggal ? dayjs(tanggal).format("DD MMMM YYYY") : "-",
+      },
+    ],
+    [classifierData]
+  );
 
   return (
     <section className="h-full">
@@ -189,30 +227,48 @@ export default function DashboardContainer() {
 
       <section className="h-auto pb-4 md:pb-0">
         {/* Dashboard View Data Surat */}
-        <div className="grid grid-cols-1  gap-4 mt-4 md:grid-cols-3">
-          {datas.map((data, index) => (
-            <div className={`bg-white shadow-xs h-36 rounded-lg p-5 ${data.rolesAllowed?.includes(roleId) ? "" : "hidden"}`} key={index}>
-              <div className="flex flex-col justify-between h-full">
-                <span className="font-semibold text-lg md:text-xl">{data.title}</span>
-                <div className="flex items-center justify-between">
-                  <span className="text-4xl md:text-5xl font-bold">{data.count ?? 0}</span>
-                  <div className="bg-blue-500 text-white p-3 rounded-lg">
-                    {data.icon}
+        <div
+          className={`grid grid-cols-1 gap-4 mt-4 ${[4].includes(roleId) || isDekan
+              ? "md:grid-cols-2"
+              : "md:grid-cols-3"
+            }`}
+        >
+          {datas.map((data, index) => {
+            if (isDekan && data.title === "Surat Disposisi") {
+              return null;
+            }
+            return (
+              <div
+                className={`bg-white shadow-xs h-36 rounded-lg p-5 ${data.rolesAllowed?.includes(roleId) ? "" : "hidden"
+                  }`}
+                key={index}
+              >
+                <div className="flex flex-col justify-between h-full">
+                  <span className="font-semibold text-lg md:text-xl">
+                    {data.title}
+                  </span>
+                  <div className="flex items-center justify-between">
+                    <span className="text-4xl md:text-5xl font-bold">
+                      {data.count ?? 0}
+                    </span>
+                    <div className="bg-blue-500 text-white p-3 rounded-lg">
+                      {data.icon}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         {/* Data Surat hari ini */}
         <div className="bg-white w-full h-full flex flex-col p-5 rounded-lg mt-5 flex-grow min-h-0">
-
           {/* Sub Header */}
           <SubHeader subHeaderTitle="Data Surat Hari Ini" />
 
           {/* Table Data */}
-          <div className="overflow-y-hidden flex-grow mt-5 min-h-0"
+          <div
+            className="overflow-y-hidden flex-grow mt-5 min-h-0"
             style={{
               maxHeight: "calc(100vh - 250px)",
             }}
@@ -224,6 +280,6 @@ export default function DashboardContainer() {
           </div>
         </div>
       </section>
-    </section >
+    </section>
   );
 }
